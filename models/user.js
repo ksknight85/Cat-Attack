8b2209918835767fbba0bc97cba8673cd3220a06
dylecl <dylann.leclairr@gmail.com>
2019-04-14 13:03:50 -0600

    added name and wins colomn to the user table

--- models/user.js
+++ models/user.js
@@ -15,6 +15,15 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    wins: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      allowNull: false
     }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
