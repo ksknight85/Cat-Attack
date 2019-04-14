e47995dfa88ea51ccd2a484c1358d41d1c72ef2a
dylecl <dylann.leclairr@gmail.com>
2019-04-14 13:24:21 -0600

    added join for users/gifs

--- models/user.js
+++ models/user.js
@@ -35,5 +35,14 @@ module.exports = function(sequelize, DataTypes) {
   User.addHook("beforeCreate", function(user) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
+
+  User.associate = function(models) {
+    // Associating Users with Gifs
+    // When an User is deleted, also delete any associated gifs
+    User.hasMany(models.Gif, {
+      onDelete: "cascade"
+    });
+  };
+
   return User;
 };
