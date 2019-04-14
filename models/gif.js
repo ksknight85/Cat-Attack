c53b79020955b0e9895a7a4a68b2d3d1dbdc177d
ksknight <korvette_00Whotmail.com>
2019-04-14 13:27:28 -0600

    added gif table

---
+++ models/gif.js
@@ -0,0 +1,27 @@
+module.exports = function(sequelize, DataTypes) {
+  var Gif = sequelize.define("Gif", {
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    wins: {
+      type: DataTypes.INTEGER,
+      default: 0,
+    }
+  });
+
+  Gif.associate = function(models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    Gif.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Post;
+};
