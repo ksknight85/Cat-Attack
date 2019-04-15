277aa297243bb43f623cea3cb3b951a3ce280e1f
dylecl <dylann.leclairr@gmail.com>
2019-04-15 11:33:15 -0600

    fixed a link

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -29,7 +29,7 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
-  // app.get("/*", function(req, res) {
+  // app.get("/", function(req, res) {
   //   res.sendFile(path.join(__dirname, "../public/index.html"));
   // });
 
