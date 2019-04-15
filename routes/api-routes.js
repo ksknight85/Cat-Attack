791f791a44ddca9a939c275a88a34eed28459864
jlcampbell16 <45213854+Jlcampbell16@users.noreply.github.com>
2019-04-15 09:52:05 -0600

    members link

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -49,7 +49,10 @@ module.exports = function(app) {
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
         email: req.user.email,
-        id: req.user.id
+        id: req.user.id,
+        firstName: req.user.firstName,
+        lastName: req.user.lastName,
+        wins: req.user.wins
       });
     }
   });
